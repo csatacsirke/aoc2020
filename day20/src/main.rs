f91e0191f9e6d5d8dfaf67ba59b0437a3615b06d
Battlechicken <battlechicken74@gmail.com>
2020-12-29 20:59:35 +0100

    day20

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,4 +1,4 @@
-use std::{hash::Hash, unimplemented, vec};
+use std::{hash::Hash, vec};
 
 use lib_aoc::*;
 
@@ -24,11 +24,22 @@ impl NormalizeEdge for Edge {
     fn normalize_edge(&self) -> Self {
         let reversed: Self = self.iter().cloned().rev().collect::<Vec<bool>>();
         let normal = self.clone();
+
+        for (&a, &b) in normal.iter().zip(&reversed) {
+            if a != b {
+                if a < b {
+                    return normal;
+                } else {
+                    return reversed;
+                }
+            }
+        }
         
-        return match normal < reversed {
-            true => normal,
-            false => reversed,
-        };
+        return normal;
+        // return match normal < reversed {
+        //     true => normal,
+        //     false => reversed,
+        // };
     }
 }
 
@@ -231,15 +242,18 @@ fn part2(input: &Vec<String>) -> String {
         debug_assert!(i != 7);
     }
 
-
+    
+    
     let mut position_to_tile_id_mapping = HashMap::<(usize, usize), TileId>::new();
-
+    
     position_to_tile_id_mapping.insert((0,0), corner_tile_id);
-
-
+    
     paint(&mut picture, (0, 0), &corner_tile_data);
-    for i in 0..tile_count_in_one_direction+1 {
-        let &prev_id = position_to_tile_id_mapping.get(&(0, i)).unwrap();
+    
+    tile_datas.insert(corner_tile_id, corner_tile_data);
+
+    for i in 1..tile_count_in_one_direction {
+        let &prev_id = position_to_tile_id_mapping.get(&(i - 1, 0)).unwrap();
         let tile_data = &tile_datas[&prev_id];
         let right_edge = get_right_edge(&tile_data);
         let &next_tile_id = 
@@ -255,12 +269,147 @@ fn part2(input: &Vec<String>) -> String {
             return left_edge == right_edge;
         });
 
+        paint(&mut picture, (i, 0), &next_tile_data);
         tile_datas.insert(next_tile_id, next_tile_data);
         
-        position_to_tile_id_mapping.insert((0, i+1), next_tile_id);
+        position_to_tile_id_mapping.insert((i, 0), next_tile_id);
     }
 
+    for j in 1..tile_count_in_one_direction {
+        for i in 0..tile_count_in_one_direction {
+            let &prev_id = position_to_tile_id_mapping.get(&(i, j - 1)).unwrap();
+            let tile_data = &tile_datas[&prev_id];
+            let bottom_edge = get_bottom_edge(&tile_data);
+            let &next_tile_id = 
+                edge_to_tile_mapping[&bottom_edge.normalize_edge()]
+                    .iter()
+                    .find(|&&id| id != prev_id)
+                    .unwrap();
+
+
+            let next_tile_data = &tile_datas[&next_tile_id];
+            let next_tile_data = rotate_until(next_tile_data, |data| {
+                let top_edge = get_top_edge(data);
+                return top_edge == bottom_edge;
+            });
+
+            paint(&mut picture, (i, j), &next_tile_data);
+            
+            tile_datas.insert(next_tile_id, next_tile_data);
+            
+            position_to_tile_id_mapping.insert((i, j), next_tile_id);
+        }
+    }
+    
+    
+    //print_picture(&picture);
+
+    for i in 0..8 {
+        if let Some(roughness) = find_water_roughness(&picture) {
+            return roughness.to_string();
+        }
+
+        // println!();
+        // print_picture(&picture);
+        // println!();
+        // println!("roughness: {}", roughness);
+
+
+        if i == 3 {
+            picture = flip(&picture);
+        } else {
+            picture = rotate(&picture);
+        }
+    }
+    
+    
     panic!();
+}
+
+
+fn find_water_roughness(picture: &PixelData) -> Option<usize> {
+    
+    let pattern = [
+        "                  # ", 
+        "#    ##    ##    ###", 
+        " #  #  #  #  #  #   "
+    ];
+
+    
+    let pattern: PixelData = 
+        pattern
+            .iter()
+            .map(|&s| to_bool_vec(s))
+            .collect();
+    
+    debug_assert!(is_sea_monser_at(&pattern, &pattern, (0, 0)));
+    debug_assert!(!is_sea_monser_at(&pattern, &pattern, (1, 0)));
+    
+
+    //print_picture(&pattern);
+
+
+    let lx = picture[0].len();
+    let ly = picture.len();
+
+    let mut monster_count = 0;
+    let mut terrain_count = 0;
+    for j in 0..ly {
+        for i in 0..lx {
+            if is_sea_monser_at(picture, &pattern, (i, j)) {
+                monster_count += 1;
+            }
+
+            if picture[j][i] {
+                terrain_count += 1;
+            }
+        }
+    }
+
+
+    if monster_count > 0 {
+        return Some(terrain_count - monster_count * 15); 
+    }
+
+    return None;
+}
+
+fn is_sea_monser_at(picture: &PixelData, pattern: &PixelData, (x, y): (usize, usize)) -> bool {
+
+    if x + pattern[0].len() > picture[0].len() {
+        return false;
+    }
+
+    if y + pattern.len() > picture.len() {
+        return false;
+    }
+
+    let lx = pattern[0].len();
+    let ly = pattern.len();
+
+    for j in 0..ly {
+        for i in 0..lx {
+            let pix_pic = picture[j + y][i + x];
+            let pix_pattern = pattern[j][i];
+            if pix_pattern {
+                if !pix_pic {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+#[allow(dead_code)]
+fn print_picture(picture: &PixelData) {
+    for row in picture {
+        for pixel in row {
+            print!("{}", match pixel { false => '.', _ => '#'});
+        }
+        println!();
+    }
 }
 
 fn rotate_until<P>(data: &PixelData, pred: P) -> PixelData
@@ -268,17 +417,23 @@ fn rotate_until<P>(data: &PixelData, pred: P) -> PixelData
 {
     let mut data = data.clone();
 
+    if pred(&data) {
+        return data;
+    }
+
     for i in 0..8 {
         
-        if pred(&data) {
-            break;
-        }
-
         if i == 4 {
             data = flip(&data);
         } else {
             data = rotate(&data);
         }
+
+        
+        if pred(&data) {
+            break;
+        }
+
         debug_assert!(i != 7);
     }
 
@@ -314,11 +469,11 @@ type Pixel = bool;
 type PixelData = Vec<Vec<Pixel>>;
 
 
-fn to_bool_vec(s: &String) -> Vec::<bool> {
+fn to_bool_vec(s: &str) -> Vec::<bool> {
     s.chars()
         .map(|c| match c {
-            '.' => false,
-            _ => true,
+            '#' => true,
+            _ => false,
         })
         .collect()
 }
@@ -326,12 +481,12 @@ fn to_bool_vec(s: &String) -> Vec::<bool> {
 
 fn paint(picture_data: &mut PixelData, (tile_i, tile_j): (usize, usize), tile_data: &PixelData) {
     let tile_width = tile_data.len();
-    let offset_x = tile_width * tile_i;
-    let offset_y = tile_width * tile_j;
+    let offset_x = (tile_width - 2) * tile_i;
+    let offset_y = (tile_width - 2) * tile_j;
 
-    for j in 0..tile_width {
-        for i in 0..tile_width {
-            picture_data[offset_y + j][offset_x + i] =
+    for j in 1..tile_width-1 {
+        for i in 1..tile_width-1 {
+            picture_data[offset_y + j - 1][offset_x + i - 1] =
                 tile_data[j][i]; 
         }
     }
